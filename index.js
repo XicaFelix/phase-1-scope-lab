891a76dcab8ac95116cfed3e6989119b1c779edf
Xica Felix <xicasfelix@gmail.com>
2021-12-01 16:28:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName(name){
+    newName = name.toUpperCase();
+    return newName;
+}
+
+upperCaseCustomerName(customerName);
